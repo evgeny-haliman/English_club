5adb73fa12306b90290cfa7dbf51dce6ff78eef3
Yevhen Khaliman <Eugen.Haliman@gmail.com>
2019-10-28 23:57:21 +0200

    add vuejs_boot

---
+++ backend/src/test/java/ml/khaliman/englishclub/EnglishClubApplicationTests.java
@@ -0,0 +1,13 @@
+package ml.khaliman.englishclub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnglishClubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
