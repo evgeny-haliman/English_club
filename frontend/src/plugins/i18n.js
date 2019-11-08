3c178b29593727a2bcc342b438c4c7dddb7854b6
Yevhen Khaliman <Eugen.Haliman@gmail.com>
2019-11-08 19:17:08 +0200

    add translate

--- frontend/src/plugins/i18n.js
+++ frontend/src/plugins/i18n.js
@@ -11,8 +11,16 @@ const messages = {
     signUp: 'Sign up',
     signIn: 'Sign in',
     h1: 'Getting started in a foreign language hasn’t been so easy',
-    h2: 'It can be done here and now'
-
+    h2: 'It can be done here and now',
+    whyImportant: 'Why it is important',
+    whatNeed: 'What is needed for this',
+    whyImportantText: 'Any business needs practice. And in the study of language, in addition to theory, a huge role is ' +
+      'played by the ability to apply your knowledge while talking with another person. On a walk or at home, at work or ' +
+      'on the road - there will always be time for doing what you love!',
+    whatNeedText: 'For a conversation, a regular computer is also suitable, of course, with a microphone. A smartphone or ' +
+      'tablet is even better, because we use them most often for calls. Well, the presence of your favorite application, with ' +
+      'the help of which you will increase the level of your knowledge of a foreign language. After registration, you can call ' +
+      'other participants or standby.'
   },
   'ua': {
     welcomeMsg: 'Доброго ранку',
@@ -21,7 +29,16 @@ const messages = {
     signUp: 'Реєстрація',
     signIn: 'Увійти',
     h1: 'Почати спілкуватися на іноземній мові ще не було так просто',
-    h2: 'Це можно зробити просто зараз'
+    h2: 'Це можно зробити просто зараз',
+    whyImportant: 'Чому це важливо',
+    whatNeed: 'Що для цього потрібно',
+    whyImportantText: 'У кожній справі потрібна практика. А у вивченні мови окрім теорії величезну роль грає можливість ' +
+      'застосувати свої знання під час розмови з іншою людиною. На прогулянці або вдома, на роботі або в дорозі - завжди ' +
+      'знайдеться час для улюбленої справи!',
+    whatNeedText: 'Для проведення розмови згодиться і звичайний компʼютер, авжеж, за наявності мікрофону. Смартфон або ' +
+      'планшет - ще краще, адже саме їх ми частіше за всього використовуємо для дзвінків. Ну і наявність улюбленої ' +
+      'програми , за допомогою якої ви і будете підвищувати свій рівень володіння іноземною мовою. Після регістрації ви зможете ' +
+      'дзвонити іншим учасникам або стати у режим очікування.'
   },
   'ru': {
     home: 'Главная',
@@ -29,7 +46,16 @@ const messages = {
     signUp: 'Регистрация',
     signIn: 'Войти',
     h1: 'Начать общаться на иностранном языке еще не было так просто ',
-    h2: 'Это можно сделать здесь и сейчас'
+    h2: 'Это можно сделать здесь и сейчас',
+    whyImportant: 'Почему это важно',
+    whatNeed: 'Что для этого нужно',
+    whyImportantText: 'В любом деле нужна практика. А в изучении языка помимо теории огромную роль играет возможность ' +
+      'применить свои знания во время разговора с другим человеком. На прогулке или дома, на работе или в дороге - всегда ' +
+      'найдется время для занятия любимым делом!',
+    whatNeedText: 'Для проведения разговора подойдет и обычный компьютер, конечно же, при наличии микрофона. Смартфон или ' +
+      'планшет - ещё лучше, ведь именно их мы чаще всего используем для звонков. Ну и наличие любимого приложения, с помощью ' +
+      'которого вы и будете повышать уровень своего владения иностранным языком. После регистрации вы сможете позвонить ' +
+      'другим участникам или стать в режим ожидания.'
   }
 }
 
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -4,15 +4,11 @@
     <div class="header"><h1>{{ $t('h1') }}</h1>
     <h2>{{ $t('h2') }}</h2></div>
     <br>
-    <div class="main"><div style="flex-basis:40%; flex-grow:0; flex-shrink:0;"><h3>Почему это важно</h3><img src="../assets/headphone.png"
+    <div class="main"><div style="flex-basis:40%; flex-grow:0; flex-shrink:0;"><h3>{{$t('whyImportant')}}</h3><img src="../assets/headphone.png"
                                                                                                              alt="Headphone">
-    <p>В любом деле нужна практика. А в изучении языка помимо теории огромную роль играет возможность применить свои знания
-      во время разговора с другим человеком. На прогулке или дома, на работе или в дороге - всегда найдется время для занятия любимым делом!
-    </p></div>
-      <div style="flex-basis:40%; flex-grow:0; flex-shrink:0;"><h3>Что для этого нужно</h3><img src="../assets/viber.png" alt="viber">
-      <p>Для проведения разговора подойдет и обычный компьютер, конечно же, при наличии микрофона. Смартфон или планшет - ещё лучше,
-      ведь именно их мы чаще всего используем для звонков. Ну и наличие любимого приложения, с помощью которого вы и будете повышать
-      уровень своего владения иностранным языком. После регистрации вы сможете позвонить другим участникам или стать в режим ожидания.</p></div>
+    <p>{{$t(('whyImportantText'))}}</p></div>
+      <div style="flex-basis:40%; flex-grow:0; flex-shrink:0;"><h3>{{$t('whatNeed')}}</h3><img src="../assets/viber.png" alt="viber">
+      <p>{{$t(('whatNeedText'))}}</p></div>
     </div>
   </div>
 </template>
